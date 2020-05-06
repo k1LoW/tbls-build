e88506862867bce8ea19de4f449b77139056d7e6
k1LoW <k1lowxb@gmail.com>
2020-05-06 23:23:31 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/k1LoW/tbls-build
+
+go 1.13
+
+require github.com/spf13/cobra v1.0.0
---
+++ version/version.go
@@ -0,0 +1,7 @@
+package version
+
+// Name for this
+const Name string = "tbls-build"
+
+// Version for this
+var Version = "dev"
